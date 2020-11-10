fe17a2436cbdec215c0805d07458c458efa221ea
Tejash Patel <tejashpatel3402@gmail.com>
2020-11-09 22:57:36 -0500

    Revert "Update GameModel.java"
    This reverts commit a317aa3f8dc2badfadf859405af6518874325d24.

--- src/game/risk/controller/GameModel.java
+++ src/game/risk/controller/GameModel.java
@@ -6,58 +6,37 @@ import java.util.Collections;
 import game.risk.model.Continent;
 import game.risk.model.Country;
 
+/**
+ * GAME'S MODEL
+ */
 public class GameModel {
 
-	private ArrayList<Country> countries;
-	private ArrayList<Continent> continents;
-	private DataFetcher fetcher;
-	
-	/**
-	 * Constructor of GameModel.
-	 */
-	public GameModel() {
-		
-		fetcher = new DataFetcher();
-		this.continents = fetcher.getContinents();
-		this.countries = fetcher.getCountries();
-		
-	}
-	
-	/**
-	 * Gets the list of countries.
-	 * @return
-	 */
+    private ArrayList<Country> countries;
+    private ArrayList<Continent> continents;
+    private DataFetcher fetcher;
 
-	public ArrayList<Country> getCountries() {
-		return countries;
-	}
+    public GameModel() {
 
-	/**
-	 * Gets the list of continents.
-	 * @return
-	 */
-	public ArrayList<Continent> getContinents() {
-		return continents;
-	}
-	
-	/**
-	 * Gets fetcher from DataFetcher.
-	 * @return
-	 */
+        fetcher = new DataFetcher();
+        this.continents = fetcher.getContinents();
+        this.countries = fetcher.getCountries();
 
-	public DataFetcher getFetcher() {
-		return fetcher;
-	}
-	 
-	/**
-	 * Shuffles the country list.
-	 */
-	public void shuffleCountries() {
-		Collections.shuffle(countries);
-	}
-	
-}
+    }
 
+    public ArrayList<Country> getCountries() {
+        return countries;
+    }
 
+    public ArrayList<Continent> getContinents() {
+        return continents;
+    }
+
+    public DataFetcher getFetcher() {
+        return fetcher;
+    }
+
+    public void shuffleCountries() {
+        Collections.shuffle(countries);
+    }
 
 }
