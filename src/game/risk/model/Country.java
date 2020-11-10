94289cba7038c867a983e3f3c8fde6c9d0b933b6
Tejash Patel <tejashpatel3402@gmail.com>
2020-11-09 22:57:27 -0500

    Revert "Update Country.java"
    This reverts commit c6cbc5b2cf5446b2e2ddcfc58e0c0511979c3545.

--- src/game/risk/model/Country.java
+++ src/game/risk/model/Country.java
@@ -3,133 +3,70 @@ package game.risk.model;
 import java.util.ArrayList;
 
 public class Country implements Comparable<Country> {
-	
-	/**
-	*GETS AND SETS COUNTRIES, JOINING AND OCCUPANT. SETS, GETS AND UPDATES ARMIES.
-	*/
 
+    // Attributes..
+    private String name;
+    private ArrayList<Country> joining;
+    private int armies;
+    private Player occupant;
 
-	// Attributes..
-	private String name;
-	private ArrayList<Country> joining;
-	private int armies;
-	private Player occupant;
-	
-	/**
-	 * Constructor for country.
-	 * Takes a name and a joining.
-	 * @param name
-	 * @param joining
-	 */
-	
-	public Country(String name, ArrayList<Country> joining) {
-		
-		this.name = name;
-		this.joining = joining;
-		
-	}
-	
-	/**
-	 * Constructor for country.
-	 * Takes a name.
-	 * @param name
-	 */
-	public Country(String name) {
-			
-		this.name = name;
-		this.joining = new ArrayList<>();
-	
-	}
-	
-	/**
-	 * Updates the number of armies by the number of armies that should be added and adding it to the armies already existing.
-	 * @param updator
-	 */
-	
-	public void updateArmies(int updator) {
-		this.armies += updator;
-	}
-	
-	/**
-	 * Gets the name of the country.
-	 * @return
-	 */
+    // Country..
+    public Country(String name, ArrayList<Country> joining) {
 
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * Gets the list of countries joining.
-	 * @return
-	 */
+        this.name = name;
+        this.joining = joining;
 
-	public ArrayList<Country> getJoining() {
-		return joining;
-	}
-	
-	/**
-	 * Gets the number of armies.
-	 * @return
-	 */
+    }
 
-	public int getArmies() {
-		return armies;
-	}
-	
-	/**
-	 * Gets the occupant player.
-	 * @return
-	 */
+    // Country..
+    public Country(String name) {
 
-	public Player getOccupant() {
-		return occupant;
-	}
-	
-	/**
-	 * Sets the name of the country.
-	 * @param name
-	 */
+        this.name = name;
+        this.joining = new ArrayList<>();
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * Sets the joining of list of countries.
-	 * @param joining
-	 */
+    }
 
-	public void setJoining(ArrayList<Country> joining) {
-		this.joining = joining;
-	}
-	
-	/**
-	 * Sets the number of armies.
-	 * @param armies
-	 */
+    public void updateArmies(int updator) {
+        this.armies += updator;
+    }
 
-	public void setArmies(int armies) {
-		this.armies = armies;
-	}
-	
-	/**
-	 * Sets the occupant player.
-	 * @param occupant
-	 */
+    public String getName() {
+        return name;
+    }
 
-	public void setOccupant(Player occupant) {
-		this.occupant = occupant;
-	}
+    public ArrayList<Country> getJoining() {
+        return joining;
+    }
 
-	@Override
-	public int compareTo(Country o) {
-		
-		return this.name.compareTo(o.getName());
-	
-	}
-	
-}
+    public int getArmies() {
+        return armies;
+    }
 
+    public Player getOccupant() {
+        return occupant;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setJoining(ArrayList<Country> joining) {
+        this.joining = joining;
+    }
+
+    public void setArmies(int armies) {
+        this.armies = armies;
+    }
+
+    public void setOccupant(Player occupant) {
+        this.occupant = occupant;
+    }
+
+    @Override
+    public int compareTo(Country o) {
+
+        return this.name.compareTo(o.getName());
+
+    }
 
 }
