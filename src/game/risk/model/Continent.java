5739cb5224fcaaa75c9510640a91a9c1ea0ce834
lynnmehyou <55845144+lynnmehyou@users.noreply.github.com>
2020-11-09 22:15:35 -0500

    Update Continent.java

--- src/game/risk/model/Continent.java
+++ src/game/risk/model/Continent.java
@@ -4,31 +4,62 @@ import java.util.ArrayList;
 
 public class Continent {
 
-    //
-    private String name;
-    private ArrayList<Country> countries;
+	
+	/**
+	 * GETS AND SETS THE CONTINENTS AND COUNTRIES FOR EACH CONTINENT.
+	 */
 
-    public Continent(String name, ArrayList<Country> countries) {
+	private String name;
+	private ArrayList<Country> countries;
+	/**
+	 * Constructor
+	 * @param name
+	 * @param countries
+	 */
+	public Continent(String name, ArrayList<Country> countries) {
+	
+		this.name = name;
+		this.countries = countries;
+	
+	}
 
-        this.name = name;
-        this.countries = countries;
+	/**
+	 * Gets the name of the continent.
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Get an array list of the countries for the continent.
+	 * @return
+	 */
+	
+	public ArrayList<Country> getCountries() {
+		return countries;
+	}
+	
+	/**
+	 * Sets the name of the continent.
+	 * @param name
+	 */
 
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Sets a list of names of countries fo the continent.
+	 * @param countries
+	 */
 
-    public String getName() {
-        return name;
-    }
+	public void setCountries(ArrayList<Country> countries) {
+		this.countries = countries;
+	}
+	
+}
 
-    public ArrayList<Country> getCountries() {
-        return countries;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCountries(ArrayList<Country> countries) {
-        this.countries = countries;
-    }
 
 }
