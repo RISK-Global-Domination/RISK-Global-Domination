19926ab234f953a429ac93fc1c5bf4f0c06ac707
Tejash Patel <tejashpatel3402@gmail.com>
2020-11-09 22:57:31 -0500

    Revert "Update Continent.java"
    This reverts commit 5739cb5224fcaaa75c9510640a91a9c1ea0ce834.

--- src/game/risk/model/Continent.java
+++ src/game/risk/model/Continent.java
@@ -4,62 +4,31 @@ import java.util.ArrayList;
 
 public class Continent {
 
-	
-	/**
-	 * GETS AND SETS THE CONTINENTS AND COUNTRIES FOR EACH CONTINENT.
-	 */
+    //
+    private String name;
+    private ArrayList<Country> countries;
 
-	private String name;
-	private ArrayList<Country> countries;
-	/**
-	 * Constructor
-	 * @param name
-	 * @param countries
-	 */
-	public Continent(String name, ArrayList<Country> countries) {
-	
-		this.name = name;
-		this.countries = countries;
-	
-	}
+    public Continent(String name, ArrayList<Country> countries) {
 
-	/**
-	 * Gets the name of the continent.
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * Get an array list of the countries for the continent.
-	 * @return
-	 */
-	
-	public ArrayList<Country> getCountries() {
-		return countries;
-	}
-	
-	/**
-	 * Sets the name of the continent.
-	 * @param name
-	 */
+        this.name = name;
+        this.countries = countries;
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * Sets a list of names of countries fo the continent.
-	 * @param countries
-	 */
+    }
 
-	public void setCountries(ArrayList<Country> countries) {
-		this.countries = countries;
-	}
-	
-}
+    public String getName() {
+        return name;
+    }
 
+    public ArrayList<Country> getCountries() {
+        return countries;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountries(ArrayList<Country> countries) {
+        this.countries = countries;
+    }
 
 }
