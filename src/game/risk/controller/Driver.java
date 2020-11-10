85dc62aeabbb82d86985e69169b093e236555c3b
Tejash Patel <tejashpatel3402@gmail.com>
2020-11-09 22:57:45 -0500

    Revert "Update Driver.java"
    This reverts commit 8c064d444c1139b905140a32cc4b9ec927eaacf2.

--- src/game/risk/controller/Driver.java
+++ src/game/risk/controller/Driver.java
@@ -7,12 +7,10 @@ import game.risk.view.GameView;
  */
 public class Driver {
 
-    /**
-	 * Opens and runs through the game sequence.
-	 * @param args
-	 */
+    //
     public static void main(String[] args) {
 
+        //
         GameView view = new GameView();
         GameModel model = new GameModel();
         GameController controller = new GameController(model, view);
