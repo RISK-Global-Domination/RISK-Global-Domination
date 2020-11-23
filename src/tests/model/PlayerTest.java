7240a9a4c01cb2054dc6ec3f58a7b108b863b9e6
Tejash Patel <tejashpatel3402@gmail.com>
2020-11-23 07:56:09 -0500

    Update PlayerTest.java

--- src/tests/model/PlayerTest.java
+++ src/tests/model/PlayerTest.java
@@ -9,13 +9,13 @@ public class PlayerTest {
 
     @org.junit.Test
     public void getName() {
-        Player p = new Player("John", 5);
+        Player p = new Player("John", 5 , 1);
         assertEquals("John" ,p.getName());
     }
 
     @org.junit.Test
     public void getArmies() {
-        Player p = new Player("John", 5);
+        Player p = new Player("John", 5, 1);
         assertEquals(5 ,p.getArmies());
     }
 }
